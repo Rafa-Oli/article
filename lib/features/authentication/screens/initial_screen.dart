bdbf5dac66236c93a909fd52fd8019aa0052f579
Rafaela Santos <rafaela.albaniza@gmail.com>
2022-08-05 14:31:22 -0300

    refactor: refactor initial screen

--- lib/features/authentication/screens/initial_screen.dart
+++ lib/features/authentication/screens/initial_screen.dart
@@ -11,6 +11,7 @@ class InitialScreen extends StatefulWidget {
 class _InitialScreenState extends State<InitialScreen> {
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Container(
       decoration: const BoxDecoration(
         color: Color.fromARGB(255, 3, 57, 100),
@@ -19,13 +20,39 @@ class _InitialScreenState extends State<InitialScreen> {
         backgroundColor: Colors.transparent,
         body: Center(
           child: Container(
+              // color: Colors.white,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
               width: 320,
               height: 370,
-              color: Colors.white,
-              child: Column(
-                children: const [
-                  Expanded(child: Text('Bem vindo ao design Diário'))
-                ],
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Bem vindo ao Design Diário',
+                      style: theme.textTheme.headline6!
+                          .copyWith(fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Entre com sua conta ou crie uma nova para acessar o conteúdo',
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.subtitle1!
+                            .copyWith(fontWeight: FontWeight.normal),
+                      ),
+                    )
+                  ],
+                ),
               )),
         ),
       ),
