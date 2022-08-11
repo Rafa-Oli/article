89bc841d8524097abf9eb91c6b05a4e93bad76fc
Rafaela Santos <rafaela.albaniza@gmail.com>
2022-08-11 08:22:09 -0300

    feat: finish screen initial

--- lib/features/authentication/screens/initial_screen.dart
+++ lib/features/authentication/screens/initial_screen.dart
@@ -20,37 +20,78 @@ class _InitialScreenState extends State<InitialScreen> {
         backgroundColor: Colors.transparent,
         body: Center(
           child: Container(
-              // color: Colors.white,
               decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(8)),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
               width: 320,
-              height: 370,
+              height: 360,
               child: Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Column(
-                  // mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      'Bem vindo ao Design Diário',
-                      style: theme.textTheme.headline6!
-                          .copyWith(fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
                     Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'Entre com sua conta ou crie uma nova para acessar o conteúdo',
-                        textAlign: TextAlign.center,
-                        style: theme.textTheme.subtitle1!
-                            .copyWith(fontWeight: FontWeight.normal),
+                      padding: const EdgeInsets.symmetric(vertical: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            'Bem vindo ao Design Diário',
+                            style: theme.textTheme.headline6!
+                                .copyWith(fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            height: 8,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Entre com sua conta ou crie uma nova para acessar o conteúdo',
+                              textAlign: TextAlign.center,
+                              style: theme.textTheme.subtitle1!
+                                  .copyWith(fontWeight: FontWeight.normal),
+                            ),
+                          ),
+                        ],
                       ),
-                    )
+                    ),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: double.maxFinite,
+                          child: ElevatedButton(
+                              style: ButtonStyle(
+                                  padding: MaterialStateProperty.all(
+                                      const EdgeInsets.all(16)),
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color.fromARGB(255, 3, 57, 100),
+                                  )),
+                              onPressed: () {},
+                              child: const Text('Acessar conta')),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        SizedBox(
+                          width: double.maxFinite,
+                          child: ElevatedButton(
+                              style: ButtonStyle(
+                                  padding: MaterialStateProperty.all(
+                                      const EdgeInsets.all(16)),
+                                  backgroundColor: MaterialStateProperty.all(
+                                    const Color.fromARGB(255, 3, 57, 100),
+                                  )),
+                              onPressed: () {},
+                              child: const Text('Criar nova conta')),
+                        )
+                      ],
+                    ),
                   ],
                 ),
               )),
